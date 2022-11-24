0bc06a2f877fb4bf2715dfc506305b6546141273
SofieEdholm90 <48571117+SofieEdholm90@users.noreply.github.com>
2022-11-24 11:10:53 +0100

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginPage from './pages/loginPage/index'
+function App() {
+	return (
+		<div className='App'>
+			<header className='App-header'>Detta är i App.tsx filen</header>
+			<LoginPage name={'Sofie'} />
+		</div>
+	)
+}
+
+export default App
